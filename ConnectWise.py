496f8e50666a9cbc308ca74f47246e9c3a493a08
sbianco-sc <108537237+sbianco-sc@users.noreply.github.com>
2025-12-01 16:31:41 -0500

    Add files via upload

---
+++ ConnectWise.py
@@ -0,0 +1,393 @@
+__version__ = '20251201.000'
+
+'''
+    Provides methods to call ConnctWise API for incident creation and update
+
+    20240205.000    initial
+    20251201.000    several updates and enhancements to get_company
+
+'''
+
+import requests
+import json
+from datetime import datetime
+
+class ConnectWise:
+
+    def __init__(self, logger, config={}, public_key=''):
+
+        self.l = logger
+        self.l.info('Stellar-ConnectWise version: [{}]'.format(__version__))
+
+        self.cw_host = config.get('cw_host')
+        self.cw_company_id = config.get('cw_company_id')
+        self.cw_private_key = config.get('cw_private_key')
+        self.cw_public_key = config.get('cw_public_key')
+        self.cw_client_id = config.get('cw_client_id')
+
+        self.cw_default_company = config['ticket']['default_company']
+        self.cw_avoid_company_lookup = config.get('ticket', {}).get('avoid_company_lookup', False)
+        self.cw_default_board = config['ticket']['default_board']
+        self.cw_use_default_board = config['ticket'].get('avoid_board_lookup', False)
+        self.ticket_prefix = config['ticket']['summary_prefix']
+        # added 20220721.000 to support ticket status
+        self.cw_ticket_status = config['ticket']['status']
+        if self.cw_ticket_status == 'New':
+            self.cw_ticket_status = ''
+
+        self.tenant_map = config['tenant_map']
+
+        # support for SLA and event_score added 20230301
+        self.sla = config.get('SLA', {})
+        self.headers = {'Accept': 'application/json', 'Content-type': 'application/json',
+                     'clientId': '{}'.format(self.cw_client_id)}
+        self.auth = ('{}+{}'.format(self.cw_company_id, self.cw_public_key), '{}'.format(self.cw_private_key))
+        self.base_url = 'https://{}/{}/apis/3.0'.format(self.cw_host, self._get_company_info())
+
+        # preload the default (fallback) company to avoid useless lookups
+        self.cw_default_company_id = self.get_default_company_id()
+
+
+    def get_version(self):
+        return __version__
+
+    def test_connection(self):
+        ret = False
+        url = "{}{}".format(self.base_url, '/system/info')
+        self.l.info("Testing connection to: [{}]".format(url))
+        r = requests.get(url=url, headers=self.headers, auth=self.auth)
+        rr = json.loads(r.text)
+        printable_r = json.dumps(rr, indent=4, sort_keys=True)
+        # self.l.debug(printable_r)
+        if 'version' in rr:
+            self.l.info("Successful connectivity test. Connectwise version: [{}]".format(rr['version']))
+            ret = True
+        else:
+            self.l.error("Connectivity test FAILED - cannot continue")
+            self.l.error("{} {}".format(r.status_code, rr))
+            raise Exception("Connectivity test FAILED - cannot continue")
+
+        return ret
+
+    def create_ticket(self, ticket_summary, company_name, board_name='', event_score=0):
+        new_ticket_id = 0
+        company_id = self.get_company(company_name)
+        (priority_name, priority_id) = self.get_ticket_priority(event_score)
+        summary_string = ticket_summary
+        if self.ticket_prefix:
+            summary_string = '{} {}'.format(self.ticket_prefix, ticket_summary)
+        if priority_name:
+            summary_string = '[{}] {}'.format(priority_name, summary_string)
+        ticket_data = {
+            'summary': '{}'.format(summary_string),
+            'company': {
+                'id': company_id
+            },
+            # 'board': {
+            # 	# 'name': '{}'.format(board_name)
+            # 	'id': board_id
+            # },
+            # added 20220721.000 to support ticket status
+            'status': {
+                'name': '{}'.format(self.cw_ticket_status)
+                # 'name': ''
+            }
+        }
+
+        if self.cw_use_default_board:
+            ticket_data['board'] = {"id": self.get_board(self.cw_default_board)}
+            # ticket_data['board'] = {"name": '{}'.format(self.cw_default_board)}
+        else:
+            ticket_data['board'] = {"id": self.get_board(board_name)}
+
+        # support for event_score and priority_id added 20230301
+        if priority_id:
+            ticket_data['priority'] = {'id': priority_id}
+        ticket_data = json.dumps(ticket_data)
+
+        url = '{}{}'.format(self.base_url, '/service/tickets')
+        r = requests.post(url=url, headers=self.headers, auth=self.auth, data=ticket_data)
+        if 200 <= r.status_code <= 299:
+            rr = json.loads(r.text)
+            new_ticket_id = int(rr['id'])
+            self.l.info("New ticket created: [{}]".format(new_ticket_id))
+        else:
+            self.l.error("Error creating ticket: [{}: {}]".format(r.status_code, r.text))
+
+        return new_ticket_id
+
+    def get_companies(self):
+        url = '{}/company/companies?fields=id,name,status&pageSize=1000'.format(self.base_url)
+        # url = '{}/company/companies?name="Microsoft"'.format(_URL_)
+        # url = '{}/company/companies?conditions=name="Microsoft"&fields=id,name,status'.format(_URL_)
+        r = requests.get(url=url, headers=self.headers, auth=self.auth)
+        if 200 <= r.status_code <= 299:
+            rr = json.loads(r.text)
+            printable_r = json.dumps(rr, indent=4, sort_keys=True)
+            # l.debug(printable_r)
+            # print(printable_r)
+            item_cnt = 0
+            for r_item in rr:
+                c_id = r_item['id']
+                c_name = r_item['name']
+                print("company id: [{}] name: [{}]".format(c_id, c_name))
+                item_cnt += 1
+        else:
+            self.l.error("Error querying companies: [{}: {}]".format(r.status_code, r.text))
+
+        print("count: {}".format(item_cnt))
+        return
+
+    def get_company(self, company_name, last_try=False, all_fields=False):
+        ret_id = 0
+        if self.cw_avoid_company_lookup:
+            self.l.info("Using default company for all tickets as optioned: [{}/{}]".format(self.cw_default_company, self.cw_default_company_id))
+            ret_id = self.cw_default_company_id
+        else:
+            self.l.info("Finding company name: [{}]".format(company_name))
+            if self.tenant_map and company_name in self.tenant_map:
+                mapped_company_name = self.tenant_map[company_name]
+                self.l.info("Tenant name: [{}] mapped to CW company: [{}]".format(company_name, mapped_company_name))
+                company_name = mapped_company_name
+            if all_fields:
+                url = '{}/company/companies?conditions=name="{}"'.format(self.base_url, company_name)
+            else:
+                url = '{}/company/companies?conditions=name="{}"&fields=id,name,status,deletedFlag'.format(self.base_url,
+                                                                                                           company_name)
+            r = requests.get(url=url, headers=self.headers, auth=self.auth)
+            if 200 <= r.status_code <= 299:
+                rr = json.loads(r.text)
+                for c in rr:
+                    if 'id' in c:
+                        df = c['deletedFlag']
+                        if df:
+                            self.l.warning("Company: [{} / {}] is flagged as deleted - skipping".format(c['id'], company_name))
+                            continue
+                        ret_id = c['id']
+                        self.l.info("Found company id: [{}]".format(ret_id))
+                        break
+            else:
+                self.l.error("Error querying companies: [{}: {}]".format(r.status_code, r.text))
+
+            if not ret_id:
+                self.l.warning("Company name [{}] could not be found. Using default company: [{}/{}]".format(
+                        company_name, self.cw_default_company, self.cw_default_company_id))
+                ret_id = self.cw_default_company_id
+
+        return ret_id
+
+    def get_default_company_id(self):
+        ret_id = 0
+        company_name = self.cw_default_company
+        if company_name:
+            self.l.info("Finding default company name: [{}]".format(company_name))
+            url = '{}/company/companies?conditions=name="{}"&fields=id,name,status,deletedFlag'.format(self.base_url, company_name)
+            r = requests.get(url=url, headers=self.headers, auth=self.auth)
+            if 200 <= r.status_code <= 299:
+                rr = json.loads(r.text)
+                for c in rr:
+                    if 'id' in c:
+                        df = c['deletedFlag']
+                        if df:
+                            self.l.warning("Company: [{} / {}] is flagged as deleted - skipping".format(c['id'], company_name))
+                            continue
+                        ret_id = c['id']
+                        self.l.info("Found company id: [{}]".format(ret_id))
+                        break
+            else:
+                self.l.error("Error querying companies: [{}: {}]".format(r.status_code, r.text))
+
+            if not ret_id:
+                raise Exception("Default company not identified: [{}] - cannot proceed".format(company_name))
+        else:
+            raise Exception("Default company name not specified; cannot proceed")
+        return ret_id
+
+    def get_boards(self):
+        self.l.info("Getting all board names")
+        url = '{}/service/boards?fields=id,name,status&pageSize=1000'.format(self.base_url)
+        r = requests.get(url=url, headers=self.headers, auth=self.auth)
+        if 200 <= r.status_code <= 299:
+            rr = json.loads(r.text)
+            item_cnt = 0
+            for c in rr:
+                if 'id' in c:
+                    ret_id = c['id']
+                    b_name = c['name']
+                    # b_status = c['status']
+                    print("Board id: [{}] name: [{}]".format(ret_id, b_name))
+                    item_cnt += 1
+
+        # printable_r = json.dumps(rr, indent=4, sort_keys=True)
+        # l.debug(printable_r)
+        # print(printable_r)
+        else:
+            self.l.error("Error querying boards: [{}: {}]".format(r.status_code, r.text))
+
+        print("count: {}".format(item_cnt))
+        return
+
+    def get_priorities(self):
+        self.l.info("Getting all priorities")
+        url = '{}/service/priorities?fields=id,name,status&pageSize=1000'.format(self.base_url)
+        r = requests.get(url=url, headers=self.headers, auth=self.auth)
+        if 200 <= r.status_code <= 299:
+            rr = json.loads(r.text)
+            item_cnt = 0
+            for c in rr:
+                if 'id' in c:
+                    ret_id = c['id']
+                    b_name = c['name']
+                    # b_status = c['status']
+                    print("Priority id: [{}] name: [{}]".format(ret_id, b_name))
+                    item_cnt += 1
+
+            printable_r = json.dumps(rr, indent=4, sort_keys=True)
+            # l.debug(printable_r)
+            print(printable_r)
+        else:
+            self.l.error("Error querying priorities: [{}: {}]".format(r.status_code, r.text))
+
+        # print("count: {}".format(item_cnt))
+        return
+
+    def get_ticket_priority(self, event_score):
+        # try:
+        event_score = int(event_score)
+        ticket_sev = ''
+        ticket_priority_id = 0
+        sla = self.sla
+        if sla:
+            if event_score >= sla['CRITICAL']['min']:
+                ticket_sev = 'CRITICAL'
+            elif event_score >= sla['HIGH']['min']:
+                ticket_sev = 'HIGH'
+            elif event_score >= sla['MED']['min']:
+                ticket_sev = 'MED'
+            else:
+                ticket_sev = 'LOW'
+            ticket_priority_id = sla[ticket_sev]['cw_priority_id']
+        # except:
+        # 	pass
+        self.l.info("Setting ticket priority: [{}: {}]".format(ticket_priority_id, ticket_sev))
+        return (ticket_sev, ticket_priority_id)
+
+    def get_board(self, board_name, last_try=False):
+        self.l.info("Finding board name: [{}]".format(board_name))
+        ret_id = 0
+        url = '{}/service/boards?conditions=name="{}"&fields=id,name,status'.format(self.base_url, board_name)
+        r = requests.get(url=url, headers=self.headers, auth=self.auth)
+        if 200 <= r.status_code <= 299:
+            rr = json.loads(r.text)
+            for c in rr:
+                if 'id' in c:
+                    ret_id = c['id']
+                    self.l.info("Found board id: [{}]".format(ret_id))
+                    break
+        # printable_r = json.dumps(rr, indent=4, sort_keys=True)
+        # l.debug(printable_r)
+        # print(printable_r)
+        else:
+            self.l.error("Error querying boards: [{}: {}]".format(r.status_code, r.text))
+
+        if not ret_id:
+            if last_try:
+                raise Exception("Service Board Name lookup failure: [{}]. Cannot create ticket.".format(board_name))
+            else:
+                # lookup the default company
+                self.l.warning("Board name [{}] could not be found. Looking up the default board: [{}]".format(board_name,
+                                                                                                          self.cw_default_board))
+                ret_id = self.get_board(self.cw_default_board, last_try=True)
+
+        return ret_id
+
+    def create_ticket_note(self, ticket_id, ticket_note_text):
+        _URL_ = self.base_url
+        _AUTH_ = self.auth
+        _HEADERS_ = self.headers
+        l = self.l
+        note_data = json.dumps(
+            {
+                'text': '{}'.format(ticket_note_text),
+                'ticketId': ticket_id,
+                'internalFlag': True,
+                'externalFlag': False,
+                "detailDescriptionFlag": True,
+                "internalAnalysisFlag": False,
+                "resolutionFlag": False
+            }
+        )
+        url = '{}/service/tickets/{}/notes'.format(_URL_, ticket_id)
+        r = requests.post(url=url, headers=_HEADERS_, auth=_AUTH_, data=note_data)
+        if 200 <= r.status_code <= 299:
+            rr = json.loads(r.text)
+            ticket_note_id = int(rr['id'])
+            l.info("New ticket note created: [ticket id: {} | note id: {}]".format(ticket_id, ticket_note_id))
+        else:
+            l.error("Error creating note for ticket: [{}: {}]".format(r.status_code, r.text))
+
+        return ticket_note_id
+
+    def get_ticket_notes(self, ticket_id):
+        _URL_ = self.base_url
+        _AUTH_ = self.auth
+        _HEADERS_ = self.headers
+        l = self.l
+        l.info("Getting ticket notes: [{}]".format(ticket_id))
+        url = "{}/service/tickets/{}/notes".format(_URL_, ticket_id)
+        r = requests.get(url=url, headers=_HEADERS_, auth=_AUTH_)
+        rr = json.loads(r.text)
+        # printable_r = json.dumps(rr, indent=4, sort_keys=True)
+        # l.debug(printable_r)
+        # print(printable_r)
+        return rr
+
+    def get_ticket(self, ticket_id):
+        _URL_ = self.base_url
+        _AUTH_ = self.auth
+        _HEADERS_ = self.headers
+        l = self.l
+        l.info("Getting ticket: [{}]".format(ticket_id))
+        rr = {}
+        url = "{}/service/tickets/{}".format(_URL_, ticket_id)
+        r = requests.get(url=url, headers=_HEADERS_, auth=_AUTH_)
+        if 200 <= r.status_code <= 299:
+            rr = json.loads(r.text)
+            # printable_r = json.dumps(rr, indent=4, sort_keys=True)
+            # l.debug(printable_r)
+            # print(printable_r)
+        else:
+            l.error("Error retrieving CW ticket id: {} [{}: {}]".format(ticket_id, r.status_code, r.text))
+        return rr
+
+    def create_ticket_note_text(self, case_summary :str, case_tenant_name :str, case_url :str, alerts=[]):
+        ticket_note_text = "{}\n\n{}\n\n{}\n\n".format(case_summary, case_tenant_name, case_url)
+        for alert in alerts:
+            ticket_note_text += "- {}\n".format(alert)
+        return ticket_note_text
+
+    def _get_company_info(self):
+        ret = ''
+        url = 'https://{}/login/companyinfo/{}'.format(self.cw_host, self.cw_company_id)
+        self.l.info("Obtaining codebase from: [{}]".format(url))
+        r = requests.get(url=url, headers=self.headers)
+        rr = json.loads(r.text)
+        printable_r = json.dumps(rr, indent=4, sort_keys=True)
+        self.l.debug(printable_r)
+        if 'Codebase' in rr:
+            ret = str(rr['Codebase']).strip("/")
+            self.l.info("Codebase returned: [{}]".format(ret))
+        else:
+            self.l.error("No codebase returned - cannot continue")
+            raise Exception("No codebase returned - cannot continue")
+        return ret
+
+    def datestring_to_epoch(self, datestring):
+        # dformat = "%Y-%m-%dT%H:%M:%S.%f+0000"   # broken most of the time
+        epoch_time = 0
+        dformat = "%Y-%m-%dT%H:%M:%S%z"
+        try:
+            epoch_time = int(datetime.strptime(datestring, dformat).timestamp()) * 1000
+        except(ValueError) as e:
+            self.l.error("Time conversion error: [{}]".format(e))
+        return epoch_time
---
+++ connectwise-case-sync.py
@@ -0,0 +1,177 @@
+#!/usr/bin/env python
+
+'''
+	version:		20221004.001
+	description:	connectwise integration script used to create Manage Service Tickets
+
+    20251201.000    forked branch for improved efficiency and updated syncs
+                    support for syncing CW resolution status and notes
+
+'''
+
+import argparse
+import yaml
+from ConnectWise import ConnectWise
+import STELLAR_UTIL
+from LOGGER_UTIL import logger_util
+from time import time, sleep
+import os, traceback
+import json
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-l', '--log-file', help='Write stdout to logfile', dest='logfile', default='')
+parser.add_argument('-d', '--debug', help='Turn on debug/verbose logging', dest='verbose', action='store_true')
+parser.add_argument("-c", "--config", help='use yaml config (default: cw-make-ticket.yaml)', dest='yaml_config',
+                    default='config.yaml')
+parser.add_argument('-p', '--persistent-volume',
+                    help='Path to persistent volume that contains the in-sync database and checkpoint timestamp files. \
+                     If empty, then current directory is used and if not prepended with "/", relative paths are assumed. \
+                     (NOTE: db and checkpoint files are created automatically) ', dest='data_volume', default='')
+args = parser.parse_args()
+l = logger_util(args)
+
+def get_env():
+    env_config = {}
+    cw_host = os.environ.get("CW_HOST", '')
+    cw_company_id = os.environ.get("CW_COMPANY_ID", '')
+    cw_private_key = os.getenv("CW_PRIVATE_KEY", '')
+    cw_client_id = os.getenv("CW_CLIENT_ID", '')
+    stellar_dp = os.getenv("STELLAR_DP", '')
+    stellar_user = os.getenv("STELLAR_USER", '')
+    stellar_api_key = os.getenv("STELLAR_API_KEY", '')
+    stellar_rbac_user = int(os.getenv("STELLAR_RBAC_USER", 0))
+    stellar_saas = int(os.getenv('STELLAR_SAAS', 0))
+    webhook_ingest_url = os.getenv('WEBHOOK_INGEST_URL', '')
+    webhook_ingest_key = os.getenv('WEBHOOK_INGEST_KEY', '')
+    if (cw_host and cw_company_id and cw_private_key and cw_client_id and stellar_api_key):
+        env_config['cw_host'] = cw_host
+        env_config['cw_company_id'] = cw_company_id
+        env_config['cw_private_key'] = cw_private_key
+        env_config['cw_client_id'] = cw_client_id
+        env_config['stellar_dp'] = stellar_dp
+        env_config['stellar_user'] = stellar_user
+        env_config['stellar_api_key'] = stellar_api_key
+        env_config['stellar_new_rbac_user_auth'] = stellar_rbac_user
+        env_config['stellar_saas'] = stellar_saas
+        env_config['webhook_ingest_url'] = webhook_ingest_url
+        env_config['webhook_ingest_key'] = webhook_ingest_key
+    else:
+        raise Exception("Missing environmental variables for API keys")
+    return env_config
+
+
+if __name__ == "__main__":
+
+    try:
+        with open(args.yaml_config, 'r') as config_file:
+            config = yaml.safe_load(config_file)
+            config.update(get_env())
+            l.configure(config)
+
+        STELLAR_CHECKPOINT_FILENAME = "stellar_checkpoint"
+        CW_CHECKPOINT_FILENAME = "cw_checkpoint"
+        POLL_INTERVAL = int(config.get('stellar_polling_interval', 5)) * 60
+
+        ''' syncs '''
+        CW_SYNC_RESOLVED = config.get('cw_sync_resolved', False)
+        CW_SYNC_NOTES = config.get('cw_sync_notes', False)
+
+        CW = ConnectWise(logger=l, config=config)
+        SU = STELLAR_UTIL.STELLAR_UTIL(logger=l, config=config, optional_data_path=args.data_volume)
+        LDB = STELLAR_UTIL.local_db(ticket_table_name='cw_tickets', optional_db_dir=args.data_volume)
+
+
+        ''' testing goes here '''
+        # test 1
+        # exit(0)
+
+
+        ''' main loop for processing tickets / cases '''
+        while True:
+
+            ts_start_of_loop = time()
+
+            '''                                             '''
+            ''' get previously opened CW tickets            '''
+            '''                                             '''
+            open_tickets = LDB.get_open_tickets()
+            for open_ticket in open_tickets:
+                rt_ticket_number = open_ticket.get('remote_ticket_id', '')
+                rt_ticket_last_modified = open_ticket.get('remote_ticket_last_modified', '')
+                stellar_case_id = open_ticket.get('stellar_case_id', '')
+                l.debug("Checking CW ticket: [{}] [last updated: {}]".format(rt_ticket_number, rt_ticket_last_modified))
+
+                ''' check on ticket resolution '''
+                if CW_SYNC_RESOLVED:
+
+                    ''' pull cw ticket '''
+                    cw_ticket = CW.get_ticket(rt_ticket_number)
+                    cw_status = cw_ticket.get('status', {}).get('name', '')
+                    if cw_status == "Resolved: Closed":
+                        l.info("CW ticket in resolved state [{}] | closing related stellar case: [{}]".format(rt_ticket_number, stellar_case_id))
+                        SU.resolve_stellar_case(case_id=stellar_case_id, update_alerts=True)
+                        LDB.close_ticket_linkage(stellar_case_id=stellar_case_id)
+
+                ''' check on new notes '''
+                if CW_SYNC_NOTES:
+
+                    ''' pull notes '''
+                    cw_ticket_notes = CW.get_ticket_notes(ticket_id=rt_ticket_number)
+                    for cw_ticket_note in cw_ticket_notes:
+                        cw_note_id = cw_ticket_note.get('id', 0)
+                        cw_note_text = cw_ticket_note.get('text')
+                        cw_note_ts_str = cw_ticket_note.get('_info', {}).get('lastUpdated', "2025-01-01T00:00:00Z")
+                        cw_note_ts = CW.datestring_to_epoch(cw_note_ts_str)
+                        if cw_note_ts > rt_ticket_last_modified:
+                            l.info("Updating stellar case: [{}] with ticket note id: [{}]".format(stellar_case_id, cw_note_id))
+                            SU.add_case_comment(case_id=stellar_case_id, comment=cw_note_text)
+                            LDB.update_remote_ticket_timestamp(stellar_case_id=stellar_case_id)
+
+
+            '''                                             '''
+            ''' get all STELLAR cases since last checkpoint '''
+            '''                                             '''
+
+            r = CW.test_connection()
+            NEW_CHECKPOINT_TS = int(time() * 1000)
+            CHECKPOINT_TS = int(SU.checkpoint_read(filepath=STELLAR_CHECKPOINT_FILENAME))
+
+            # cases = SU.get_stellar_cases(from_ts=1707541200000)
+            cases = SU.get_stellar_cases(from_ts=CHECKPOINT_TS, use_modified_at=False)
+            for case in cases.get('cases', {}):
+                stellar_case_id = case.get("_id")
+                stellar_case_number = case.get('ticket_id')
+                case_name = case.get('name', '')
+                case_score = case.get('score', 0)
+                case_tenant_name = case.get('tenant_name')
+                case_summary = SU.get_case_summary(case_id=stellar_case_id)
+                event_names = SU.get_case_alerts(stellar_case_id, return_only_alert_names=True)
+                stellar_url = SU.make_stellar_case_url(stellar_case_id)
+                l.info(
+                    "Stellar Case ID: [{}] | Ticket Number: [{}] | URL: [{}]".format(stellar_case_id, stellar_case_number,
+                                                                                     stellar_url))
+                new_ticket_id = CW.create_ticket(ticket_summary=case_name, company_name=case_tenant_name, event_score=case_score)
+                if new_ticket_id:
+                    ticket_note_text = CW.create_ticket_note_text(case_summary=case_summary,
+                                                                  case_tenant_name=case_tenant_name, case_url=stellar_url,
+                                                                  alerts=event_names)
+                    CW.create_ticket_note(ticket_id=new_ticket_id, ticket_note_text=ticket_note_text)
+                    stellar_comment = "Connectwise ticket created: [{}]".format(new_ticket_id)
+                    SU.update_stellar_case(case_id=stellar_case_id, case_comment=stellar_comment)
+                    LDB.put_ticket_linkage(stellar_case_id=stellar_case_id, stellar_case_number=stellar_case_number, remote_ticket_id=new_ticket_id)
+                else:
+                    l.error("Failed to create Connectwise ticket - see log messages for more information")
+
+            SU.checkpoint_write(filepath=STELLAR_CHECKPOINT_FILENAME, val=NEW_CHECKPOINT_TS)
+            ts_loop_duration = time() - ts_start_of_loop
+            if POLL_INTERVAL > ts_loop_duration:
+                ts_sleep_time = POLL_INTERVAL - ts_loop_duration
+                l.info("Process loop duration took {}s - sleeping: {}s".format(ts_loop_duration, ts_sleep_time))
+                sleep(ts_sleep_time)
+            else:
+                l.warning("Process loop duration took longer than sleep time - staying awake to catch up")
+
+    except Exception as e:
+        l.error(traceback.format_exc())
+        exit(1)
+
